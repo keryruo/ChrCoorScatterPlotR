e970da02be1259a1ab397f2b51875d927fdfdddf
keryruo <keryruo@gmail.com>
2018-05-10 17:53:35 +0800

    Update OptimalCovariateNum.R

--- OptimalCovariateNum.R
+++ OptimalCovariateNum.R
@@ -14,7 +14,7 @@ cvrtNoSelect<-function(reaDir,writeDir,N){  # N is the number of PC
            significantQ<-c(significantQ,sum(qtlReport$FDR<fdr)) # qtlReport[,6,with=FALSE] is right, qtlReport[,6] is false for data.table
         }
        }
-       sigfPeaks<-data.frame(cvrtnum=factor(rep(paste0(1:20,'PCno'),each=length(seq(0,0.25,0.01))),levels=paste0(1:20,'PCno')),sigfPeaksNum=significantQ,fdr=rep(seq(0,0.25,0.01),20))
+       sigfPeaks<-data.frame(cvrtnum=factor(rep(paste0(1:N,'PCno'),each=length(seq(0,0.25,0.01))),levels=paste0(1:N,'PCno')),sigfPeaksNum=significantQ,fdr=rep(seq(0,0.25,0.01),N))
        max_fdr01=max(sigfPeaks[sigfPeaks$fdr==0.1,'sigfPeaksNum'])
        pdf(file=paste0(writeDir,"Significant QTL number vs PC number.pdf"))
        p0<-ggplot(sigfPeaks,aes(x=fdr,y=sigfPeaksNum,colour=cvrtnum))+geom_line()+theme(plot.title = element_text(size=rel(1.5),family="Times",face="bold",hjust=0.5),axis.title=element_text(size=9,face="bold",hjust=0.5),axis.text.y=element_text(size=8),axis.text.x = element_text(hjust = 1,size=8))
