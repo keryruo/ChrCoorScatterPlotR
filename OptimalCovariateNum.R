7bb4cee74bcc3ff61bc76319a0a7b4db39926799
keryruo <keryruo@gmail.com>
2018-05-10 18:05:06 +0800

    Update OptimalCovariateNum.R

--- OptimalCovariateNum.R
+++ OptimalCovariateNum.R
@@ -16,9 +16,10 @@ cvrtNoSelect<-function(reaDir,writeDir,N){  # N is the number of PC
        }
        sigfPeaks<-data.frame(cvrtnum=factor(rep(paste0(1:N,'PCno'),each=length(seq(0,0.25,0.01))),levels=paste0(1:N,'PCno')),sigfPeaksNum=significantQ,fdr=rep(seq(0,0.25,0.01),N))
        max_fdr01=max(sigfPeaks[sigfPeaks$fdr==0.1,'sigfPeaksNum'])
-       pdf(file=paste0(writeDir,"Significant QTL number vs PC number.pdf"))
+       pdf()
        p0<-ggplot(sigfPeaks,aes(x=fdr,y=sigfPeaksNum,colour=cvrtnum))+geom_line()+theme(plot.title = element_text(size=rel(1.5),family="Times",face="bold",hjust=0.5),axis.title=element_text(size=9,face="bold",hjust=0.5),axis.text.y=element_text(size=8),axis.text.x = element_text(hjust = 1,size=8))
        p0<-p0+ggtitle("Distribution of significant SNP number")+xlab("FDR")+ylab("Number of significant SNPs")+ geom_vline(xintercept = 0.1, colour="red", linetype = "dashed")
        p0+geom_text(aes(label=ifelse(sigfPeaksNum==max_fdr01,as.character(cvrtnum),'')),hjust=1,vjust=1,colour='black',size=3)
+       ggsave(file=paste0(writeDir,"Significant QTL number vs PC number.pdf"))
        dev.off()
 }
